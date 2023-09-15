a776c85179e28b2a998b726f4f0c482ea3712c20
许钱洲 <xuqz@quantdo.com.cn>
2023-09-15 18:09:39 +0800

    fix: github白屏问题修复

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  history: { type: 'hash' },
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/smartTeemo-web/',
   routes: [
     { path: "/", component: "home" },
   ],
