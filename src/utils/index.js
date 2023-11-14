b58f61141f242b57c83dd1d496e07344a1ab12a5
许钱洲 <xuqz@quantdo.com.cn>
2023-11-14 15:32:09 +0800

    feat: 版本升级2.3.0

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,8 @@
 export const downFun = ()=> {
     let a=document.createElement('a')
     a.style.display = 'none'
-    a.setAttribute('download', 'smartTeemo Setup 1.2.0.exe')
-    a.href = require('@/assets/smartTeemo Setup 1.2.0.exe')
+    a.setAttribute('download', 'smartTeemo Setup 2.3.0.exe')
+    a.href = require('@/assets/smartTeemo Setup 2.3.0.exe')
     document.body.appendChild(a)
     console.log('href', a.href)
     a.click()
